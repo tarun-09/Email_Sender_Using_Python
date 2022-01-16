5885dcfe74db50197a11ca5826c3dfcdbd0dd3ba
Tarun Agrawal <tarunagrawalgarg@gmail.com>
2022-01-16 20:01:37 +0530

    Added About UI

--- about_UI.py
+++ about_UI.py
@@ -3,7 +3,7 @@ from PIL import ImageTk, Image
 
 bg = "#BFD3D6"
 master = Tk()
-master.geometry('500x500')
+master.geometry('500x600')
 master.configure(bg=bg)
 master.title("Email App")
 
@@ -11,9 +11,43 @@ master.title("Email App")
 titleLabel = Label(master, text="Email App", width=20, bg=bg, fg="#100A2D", font=('Calibri', 20)).place(x=100, y=10)
 Label(master, text="ABOUT", width=30, bg=bg, font=('Calibri', 12)).place(x=115, y=45)
 
-canvas = Canvas(master, width=300, height=300)
-canvas.pack()
-img = ImageTk.PhotoImage(Image.open("FingerImages/1.png"))
-canvas.create_image(20, 20, anchor=NW, image=img)
+# Image 1
+image1 = Image.open("FingerImages/1.png")
+resize_image1 = image1.resize((100, 100))
+test1 = ImageTk.PhotoImage(resize_image1)
+
+label1 = Label(image=test1)
+label1.image = test1
+label1.place(x=10, y=85)
+
+
+# Image 2
+image2 = Image.open("FingerImages/2.png")
+resize_image2 = image2.resize((100, 100))
+test2 = ImageTk.PhotoImage(resize_image2)
+
+label2 = Label(image=test2)
+label2.image = test2
+label2.place(x=10, y=205)
+
+
+# Image 3
+image3 = Image.open("FingerImages/3.png")
+resize_image3 = image3.resize((100, 100))
+test3 = ImageTk.PhotoImage(resize_image3)
+
+label3 = Label(image=test3)
+label3.image = test3
+label3.place(x=10, y=325)
+
+
+# Image 4
+image4 = Image.open("FingerImages/4.png")
+resize_image4 = image4.resize((100, 100))
+test4 = ImageTk.PhotoImage(resize_image4)
+
+label4 = Label(image=test4)
+label4.image = test4
+label4.place(x=10, y=445)
 
 master.mainloop()
