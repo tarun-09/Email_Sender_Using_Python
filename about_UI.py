63b2362d2f20772ba0552f266afda023461e7fe1
Vaibhav Goel <vaibhavgoel0230@gmail.com>
2022-02-04 12:54:33 +0530

    Added About Info Text

--- about_UI.py
+++ about_UI.py
@@ -20,6 +20,9 @@ label1 = Label(image=test1)
 label1.image = test1
 label1.place(x=10, y=85)
 
+# Info 1
+Label(master, text="Show 1 to open Login Info dialog box.\n This Dialog Box will facilitate the user to\n enter its Login Credentials.",width=46,height=5, font=('Calibri', 12)).place(x=120, y=87)
+
 
 # Image 2
 image2 = Image.open("FingerImages/2.jpg")
@@ -29,6 +32,9 @@ test2 = ImageTk.PhotoImage(resize_image2)
 label2 = Label(image=test2)
 label2.image = test2
 label2.place(x=10, y=205)
+
+# Info 2
+Label(master, text="Show 2 to open Email Compose dialog box.\n This Dialog Box will facilitate the user to\n compose Email.",width=46,height=5, font=('Calibri', 12)).place(x=120, y=207)
 
 
 # Image 3
@@ -40,6 +46,9 @@ label3 = Label(image=test3)
 label3.image = test3
 label3.place(x=10, y=325)
 
+# Info 3
+Label(master, text="Show 3 to send email to the destined User.\n Just show 3, it will automatically send\n the Email.",width=46,height=5, font=('Calibri', 12)).place(x=120, y=327)
+
 
 # Image 4
 image4 = Image.open("FingerImages/4.png")
@@ -49,5 +58,8 @@ test4 = ImageTk.PhotoImage(resize_image4)
 label4 = Label(image=test4)
 label4.image = test4
 label4.place(x=10, y=445)
+
+# Info 4
+Label(master, text="Show 4 to open About Info dialog box.\n This Dialog box will display the rules\n to run this application.",width=46,height=5, font=('Calibri', 12)).place(x=120, y=447)
 
 master.mainloop()
